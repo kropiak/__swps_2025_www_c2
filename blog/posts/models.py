5cb1b4ffc51412cbd841684380c4ffa03494a3c3
kropiak <kropiak@matman.uwm.edu.pl>
2025-11-22 18:21:49 +0100

    po zakończeniu zajęć z lab 3

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(null=True, blank=True)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+# ORM - Object Relational Mapping
