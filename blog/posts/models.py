18c722338447b8a2a6c71caaa5c30637d09da923
kropiak <kropiak@matman.uwm.edu.pl>
2025-11-23 14:47:18 +0100

    przed zadaniami w lab 4

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,10 +5,16 @@ class Category(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
 
 # ORM - Object Relational Mapping
